b39f799a95e58a0b0469cfe6d7967964b1ff0638
ayesh <ayesh.gaur@gmail.com>
2023-04-23 19:02:09 -0400

    completed scraping of recipes for one page and writing the data to excel

--- src/test/java/com/tarladala/recipe/scraping/utilities/WriteExcel.java
+++ src/test/java/com/tarladala/recipe/scraping/utilities/WriteExcel.java
@@ -8,35 +8,35 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.*;
 
 public class WriteExcel {
-public XSSFWorkbook workbook;
-public FileOutputStream fo;
-public FileInputStream fi;
-public XSSFSheet sheet;
-public XSSFRow row;
-public XSSFCell cell;
+    public XSSFWorkbook workbook;
+    public FileOutputStream fo;
+    public FileInputStream fi;
+    public XSSFSheet sheet;
+    public XSSFRow row;
+    public XSSFCell cell;
 
     public void setCellData(String sheetName, int rownum, int column, String data) throws IOException {
-        String path = System.getProperty("C:/Users/ayesh/IdeaProjects/Data_Scraping_Framework_Demo1/src/test/resources/outPutData.xlsx");
-        File xlfile =  new File(path);
-        if(!xlfile.exists()){
-         workbook = new XSSFWorkbook();
-fo = new FileOutputStream(path);
-workbook.write(fo);
+        String path ="./src/test/resources/outPutData.xlsx";
+        File xlfile = new File(path);
+        if (!xlfile.exists()) {
+            workbook = new XSSFWorkbook();
+            fo = new FileOutputStream(path);
+            workbook.write(fo);
         }
-        fi= new FileInputStream(path);
+        fi = new FileInputStream(path);
         workbook = new XSSFWorkbook(fi);
 
-        if(workbook.getSheetIndex(sheetName)==-1)
+        if (workbook.getSheetIndex(sheetName) == -1)
             workbook.createSheet(sheetName);
-        sheet=workbook.getSheet(sheetName);
+        sheet = workbook.getSheet(sheetName);
 
-        if(sheet.getRow(rownum)==null)
+        if (sheet.getRow(rownum) == null)
             sheet.createRow(rownum);
-        row=sheet.getRow(rownum);
+        row = sheet.getRow(rownum);
 
-        cell=row.createCell(column);
+        cell = row.createCell(column);
         cell.setCellValue(data);
-        fo=new FileOutputStream(path);
+        fo = new FileOutputStream(path);
         workbook.write(fo);
         workbook.close();
         fi.close();
