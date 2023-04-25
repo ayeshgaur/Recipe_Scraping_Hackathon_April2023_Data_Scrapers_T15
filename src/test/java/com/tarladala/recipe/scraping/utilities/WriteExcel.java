8726be77ff8009bf901fc58e3fe638ba470bccfa
Monika morawadkar <marihalm47@gmail.com>
2023-04-24 20:19:44 -0500

    Added seprate methods topopulate allergies

--- src/test/java/com/tarladala/recipe/scraping/utilities/WriteExcel.java
+++ src/test/java/com/tarladala/recipe/scraping/utilities/WriteExcel.java
@@ -5,10 +5,7 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class WriteExcel {
     public XSSFWorkbook workbook;
@@ -20,6 +17,33 @@ public class WriteExcel {
 
     public void setCellData(String sheetName, int rownum, int column, String data) throws IOException {
         String path ="./src/test/resources/outPutData.xlsx";
+        File xlfile = new File(path);
+        if (!xlfile.exists()) {
+            workbook = new XSSFWorkbook();
+            fo = new FileOutputStream(path);
+            workbook.write(fo);
+        }
+        fi = new FileInputStream(path);
+        workbook = new XSSFWorkbook(fi);
+
+        if (workbook.getSheetIndex(sheetName) == -1)
+            workbook.createSheet(sheetName);
+        sheet = workbook.getSheet(sheetName);
+
+        if (sheet.getRow(rownum) == null)
+            sheet.createRow(rownum);
+        row = sheet.getRow(rownum);
+
+        cell = row.createCell(column);
+        cell.setCellValue(data);
+        fo = new FileOutputStream(path);
+        workbook.write(fo);
+        workbook.close();
+        fi.close();
+        fo.close();
+    }
+    public void setCellAllergyData(String sheetName, int rownum, int column, String data) throws IOException {
+        String path ="./src/test/resources/AllergyData.xlsx";
         File xlfile = new File(path);
         if (!xlfile.exists()) {
             workbook = new XSSFWorkbook();
